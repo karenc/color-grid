aba6f5ddcbad59f8f60ea4e4b92781157a350733
karen chan <cee.wing@gmail.com>
2013-08-09 08:35:53 +0200

    Display color grids stored in server

--- cgi-bin/server.cgi
+++ cgi-bin/server.cgi
@@ -2,8 +2,8 @@
 
 def main():
     print '200 OK'
-    print 'Content-Type: text/plain\r\n\r'
-    print 'Hello!'
+    print 'Content-Type: application/json\r\n\r'
+    print '[{"color": "red", "position": 0, "id": 0}, {"color": "blue", "position": 1, "id": 1}]'
 
 if __name__ == '__main__':
     main()
--- js/color-grid.js
+++ js/color-grid.js
@@ -1,2 +1,59 @@
 (function($) {
+    $(function() {
+        window.currentColor = 'white';
+
+        var Grid = Backbone.Model.extend({
+            setColor: function(color) {
+                this.color = color;
+            }
+        });
+
+        var GridTable = Backbone.Collection.extend({
+            model: Grid
+        });
+
+        var GridView = Backbone.View.extend({
+            tagName: 'td',
+            model: Grid,
+            events: {
+                'click': 'clickHandler'
+            },
+            initialize: function() {
+                this.listenTo(this.model, 'change', this.render);
+                this.listenTo(this, 'click', this.setColor);
+            },
+            clickHandler: function() {
+                this.model.save({color: window.currentColor});
+            },
+            render: function() {
+                this.$el.css('background-color', this.model.get('color'));
+                return this;
+            }
+        });
+
+        var AppView = Backbone.View.extend({
+            tagName: 'tr',
+            initialize: function() {
+                this.grids = new GridTable();
+                this.grids.bind('all', this.render, this);
+                this.grids.fetch();
+            },
+            render: function() {
+                this.$el.html('');
+                this.grids.each(function(grid) {
+                    this.$el.append(new GridView({model: grid}).render().el);
+                }, this);
+                return this;
+            }
+        });
+
+        var app = new AppView();
+        $('#color-grid').append(app.render().el);
+    });
+
+    Backbone.sync = function(method, model, options) {
+        var xhr = options.xhr = $.ajax('cgi-bin/server.cgi?method=' + method, options);
+        model.trigger('request', model, xhr, options);
+        return xhr;
+    };
 })(jQuery);
